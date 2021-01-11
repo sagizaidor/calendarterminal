6431523ffd0fb1d918cb2f8f025ebbe1b361f396
Sagi Zaid Or <sagi@eretz.org.il>
2021-01-11 22:59:10 +0200

    23:00

--- app/interpreter.py
+++ app/interpreter.py
@@ -1,6 +1,27 @@
 import re
 from typing import Dict
 
+def check_if_hebrew(match: Dict[str, str]) -> bool:
+    alef_ad_taf = range(ord('א'), ord('ת') + 1)
+    if ord(match['command'][0]) in alef_ad_taf:
+        return True
+    return False
 
-def interapt_message(message: str) -> Dict[str]:
-    pass
+
+def hebrew_time_fixer(match: Dict[str, str]) -> Dict[str, str]:
+    copy = match.copy()
+    start = match['start']
+    end = match['end']
+    copy['start'] = end
+    copy['end'] = start
+    return copy
+
+def get_groups(message: str) -> Dict[str, str]:
+    regex = regex = r"(?P<command>.{3,4}) (?P<day>\d{1,2})\/(?P<month>\d{1,2})\/?(?P<year>\d{4}|\d{2})? ?(?P<start>\d{1,2}\:\d{2})?-?(?P<end>\d{1,2}\:\d{2})? (?P<content>.+)"
+    m = re.match(regex, message, re.MULTILINE)
+    return m.groupdict()
+
+
+def interapt_message(message: str) -> Dict[str, str]:
+    gd = get_groups(message)
+    return gd
--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -1,1 +1,35 @@
-from app.interpreter import interapt_message
+from app.interpreter import check_if_hebrew, get_groups, hebrew_time_fixer
+
+
+def test_get_groups():
+    message_dict = get_groups('הוסף 27/10 פגישה עם שגיא')
+    print(message_dict)
+    assert message_dict['command'] == 'הוסף'
+    assert message_dict['day'] == '27'
+    assert message_dict['month'] == '10'
+    assert message_dict['content'] == 'פגישה עם שגיא'
+
+
+def test_get_groups_with_start_time():
+    message_dict = get_groups('הוסף 27/10 19:00 פגישה עם שגיא')
+    assert message_dict['command'] == 'הוסף'
+    assert message_dict['day'] == '27'
+    assert message_dict['month'] == '10'
+    assert message_dict['content'] == 'פגישה עם שגיא'
+    assert message_dict['start'] == '19:00'
+
+
+def test_check_if_hebrew():
+    message_dict = get_groups('הוסף 27/10 19:00 פגישה עם שגיא')
+    assert check_if_hebrew(message_dict)
+    message_dict = get_groups('add 27/10 meeting with sagi')
+    assert not check_if_hebrew(message_dict)
+
+
+def test_hebrew_time_fixer():
+    message_dict = get_groups('add 27/10 12:00-19:00 meeting with sagi')
+    assert message_dict['start'] == '12:00'
+    message_dict = get_groups('הוסף 27/10 19:00-12:00 פגישה עם שגיא')
+    assert message_dict['start'] == '19:00'
+    message_dict = hebrew_time_fixer(message_dict)
+    assert message_dict['start'] == '12:00'
